b07f3bd7c9abdc15a26a9f9d3033e61c2b4e3bb9
arifzaidpp <arifzaidaiju@gmail.com>
2024-03-15 11:26:54 +0530

    Design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,106 @@
-import logo from './logo.svg';
 import './App.css';
+import {AiOutlineDelete, AiOutlineEdit} from 'react-icons/ai';
+import {BsCheckLg} from 'react-icons/bs';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Todos</h1>
+
+      <div className="todo-wrapper">
+        <div className="todo-input">
+          <div className="todo-input-item">
+            <label>Title</label>
+            <input
+              type="text"
+              
+              placeholder="What's the task title?"
+            />
+          </div>
+          <div className="todo-input-item">
+            <label>Description</label>
+            <input
+              type="text"
+              placeholder="What's the task description?"
+            />
+          </div>
+          <div className="todo-input-item">
+            <button
+              type="button"
+              className="primaryBtn"
+            >
+              Add
+            </button>
+          </div>
+        </div>
+
+        <div className="btn-area">
+          <button
+            className={`secondaryBtn 'active'}`}
+          >
+            Todo
+          </button>
+          <button
+            className={`secondaryBtn 'active'}`}
+          >
+            Completed
+          </button>
+        </div>
+
+        <div className="todo-list">
+
+          
+                  <div className='edit__wrapper' >
+                  <input placeholder='Updated Title' />
+                  <textarea placeholder='Updated Title' />
+                   <button
+              type="button"
+              className="primaryBtn"
+            >
+              Update
+            </button>
+              </div> 
+                 
+                  <div className="todo-list-item">
+                    <div>
+                      <h3>{}</h3>
+                      <p>{}</p>
+                    </div>
+  
+                    <div>
+                      <AiOutlineDelete
+                        className="icon"
+                        title="Delete?"
+                      />
+                      <BsCheckLg
+                        className="check-icon"
+                        title="Complete?"
+                      />
+                      <AiOutlineEdit  className="check-icon"
+                        title="Edit?" />
+                    </div>
+  
+                  </div>
+
+          
+                <div className="todo-list-item" >
+                  <div>
+                    <h3>{}</h3>
+                    <p>{}</p>
+                    <p><small>Completed on: {}</small></p>
+                  </div>
+
+                  <div>
+                    <AiOutlineDelete
+                      className="icon"
+                      title="Delete?"
+                    />
+                  </div>
+
+                </div>
+
+        </div>
+      </div>
     </div>
   );
 }
